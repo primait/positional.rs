4c4809dfa0c1a0292cd7ac68a28e81561c599fae
Denis Müller <denis.muller@prima.it>
2022-12-22 16:44:14 +0100

    Name clash test to ensure proper macro hygiene.

---
+++ positional/tests/name_clashes.rs
@@ -0,0 +1,42 @@
+pub trait ToPositionalField {}
+pub trait ToPositionalRow {}
+pub trait FromPositionalRow {}
+
+mod positional {}
+
+#[derive(::positional::ToPositionalRow, ::positional::FromPositionalRow, PartialEq, Debug)]
+struct Data {
+    #[field(size = 5)]
+    name: String,
+    #[field(size = 5, filler = '-', align = "right")]
+    age: i32,
+    #[field(size = 20)]
+    address: String,
+}
+
+impl Data {
+    pub fn new(name: impl ToString, age: i32, address: impl ToString) -> Self {
+        Self {
+            name: name.to_string(),
+            age,
+            address: address.to_string(),
+        }
+    }
+}
+
+#[test]
+fn parse() {
+    let row =
+        ::positional::FromPositionalRow::from_positional_row("1    ---10the address is this ")
+            .expect("error converting from positional row");
+    assert_eq!(Data::new(1, 10, "the address is this"), row);
+}
+
+#[test]
+fn ser_de() {
+    let data = Data::new(1, 100, "the address is this");
+    let row = ::positional::ToPositionalRow::to_positional_row(&data);
+    let original_data: Data = ::positional::FromPositionalRow::from_positional_row(&row)
+        .expect("error converting from positional row");
+    assert_eq!(original_data, data);
+}
